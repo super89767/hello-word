449e521f96ccb302b05f56bd1a85ca7293bc767b
super89767 <729113416@qq.com>
2016-04-06 15:21:34 +0800

    add file
    add file

---
+++ AdminJsonController.java
@@ -0,0 +1,23 @@
+package com.funo.framework.web.jsoncontroller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.funo.framework.web.controller.BaseController;
+
+@Controller
+@RequestMapping("/commons/json")
+public class AdminJsonController extends BaseController{
+	private static final Logger logger = Logger.getLogger(AdminJsonController.class);
+	
+	@RequestMapping("/toIn")
+	public ModelAndView toIn(HttpServletRequest request, HttpServletResponse response){
+		
+		return new ModelAndView("aatest/MyJsp");
+	}
+}
